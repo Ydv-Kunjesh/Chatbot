0f9baec503791110303b0131a8c0964f930feec6
Gulshanray <rajanshu23187@gmail.com>
2024-01-07 12:52:36 +0530

    first commit

---
+++ app_test.py
@@ -0,0 +1,18 @@
+
+import streamlit as st
+# st.image('https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80', caption='Sunrise by the mountains')
+st.markdown(
+        """
+        <style>
+            .reportview-container {background-color:Pink;}
+        </style>
+        """,
+        unsafe_allow_html=True)
+st.markdown("""
+    <div style="background-color:Yellow;padding:10px;border-radius:20px;margin:35px">
+        <h2 style="color:Black;text-align:center;">Sarvagya - Ask Whatever You Want </h2>
+    </div>
+    """, unsafe_allow_html=True)
+st.image('DOWNLOAD.png')
+"""
+API Key `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`"""
